a1c3177b513dc50613fce5b03cc7b4c24dcc93cf
Omar Ozgur <oozgur217@gmail.com>
2017-05-15 13:44:40 -0700

    Retrieve all posts when logged in

--- flock/views/loggedin_landing.js
+++ flock/views/loggedin_landing.js
@@ -19,6 +19,36 @@ export default class LoggedIn_landing extends React.Component {
     static navigationOptions = {
         title: 'Flocks Near You',
     };
+
+  constructor(props) {
+    super(props);
+    this.state = { 
+    posts: []
+    };
+  }
+
+  componentWillMount() {
+    fetch('https://flock-site-api.herokuapp.com/posts', {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+        }).then((response) => response.json())
+        .then((responseJson) => {
+          console.log(responseJson);
+          if (responseJson.status == "success") {
+            if (responseJson.posts) {
+              this.setState({'posts': responseJson.posts})
+            }
+            console.log(this.state.posts)
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+  }
+
     render() {
       const { navigate } = this.props.navigation;
         return (
@@ -39,6 +69,21 @@ export default class LoggedIn_landing extends React.Component {
   <Text style={{marginTop: 10}}>Join a flock!</Text>
   
 <ScrollView>
+
+{this.state.posts.map(function(post, i) {
+  return (
+    <Card title='Event 1'>
+      <Text style={{marginBottom: 10}}>
+        {post.Title}
+      </Text>
+      <Button backgroundColor='#03A9F4' 
+       buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+      title='Join this Flock!' 
+      onPress={()=>navigate('MapView')}
+      />
+    </Card>
+  );
+})}
 
 <Card title='Event 1'>
     <Text style={{marginBottom: 10}}>
