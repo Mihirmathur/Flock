e97965005130cefca7d3ba5a784dd4faeba54883
Omar Ozgur <oozgur217@gmail.com>
2017-05-25 16:14:57 -0700

    Fixed iOS build

--- flock/android/app/src/main/java/com/flock/MainApplication.java
+++ flock/android/app/src/main/java/com/flock/MainApplication.java
@@ -3,6 +3,7 @@ package com.flock;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.magus.fblogin.FacebookLoginPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new MapsPackage(),
             new FBSDKPackage(),
             new FacebookLoginPackage(),
             new VectorIconsPackage()
